e4108507224df4f73315153f0f6998a4c540f90e
KonTroAl <kostya98295@mail.ru>
2020-06-07 22:58:45 +0500

    Second Part of Task_4-6 Done

--- 4.py
+++ 4.py
@@ -1,16 +1,31 @@
 class Storage:
-    def __init__(self):
-        self.numTech = {}
 
     @staticmethod
-    def takeTech():
-        # my_dict = self.numTech{"name": Tech.my_func(), "value": value}
-        pass
+    def take():
 
-        # print(self.numTech)
+        a = tech_1.my_list()
+        b = tech_2.my_list()
+        c = tech_3.my_list()
+        my_dict_1 = {"position_1": a[0], "value": int(a[1])}
+        my_dict.update(my_dict_1)
+        my_dict.update(position_2=b[0], value_2=int(b[1]))
+        my_dict.update(position_3=c[0], value_3=int(c[1]))
 
-    def giveTech(self):
-        pass
+        print(my_dict)
+
+    @staticmethod
+    def give(org, name, number):
+        dict = my_dict
+        arg = 0
+        if name == dict["position_1"] or name == dict["position_2"] or name == dict["position_3"]:
+            arg = name
+            if dict["value"] > 0 or dict["value_2"] > 0 or dict["value_3"] > 0:
+                
+                print(f"{name}, {number} in {org}")
+            else:
+                print(f"{name} not on sclad")
+        else:
+            print(f"Don't have such tech!")
 
 
 class Tech:
@@ -22,21 +37,16 @@ class Tech:
     def my_func(self):
         return self.name, self.number
 
-    def show(self):
+    def my_list(self):
         self.list.append(self.name)
-        print(self.list)
+        self.list.append(self.number)
+        return self.list
+
 
 class Printer(Tech):
     def __init__(self, name, number, series):
         super().__init__(name, number)
         self.series = series
-
-    def my_func(self):
-        return self.name, self.number
-
-    # def income(self):
-    #     self.list.append(self.name)
-    #     return self.list
 
 
 class Scan(Tech):
@@ -44,27 +54,20 @@ class Scan(Tech):
         super().__init__(name, number)
         self.speed = speed
 
-    def my_func(self):
-        return self.name, self.number
-
 
 class Xerox(Tech):
     def __init__(self, name, number, value):
         super().__init__(name, number)
         self.value = value
 
-    def my_func(self):
-        return self.name, self.number
 
-
-tech = Tech("Hp", 25)
 tech_1 = Printer("HP", 25, 25)
 tech_2 = Scan("MSI", 12, 25)
 tech_3 = Xerox("Canon", 5, 25)
-print(tech_1.my_func())
-print(tech_2.my_func())
-print(tech_3.my_func())
-# tech_1.income()
-tech.show()
+# print(tech_1.my_func())
+# print(tech_2.my_func())
+# print(tech_3.my_func())
+my_dict = {}
 
-Storage.takeTech()
+Storage.take()
+Storage.give("OK", "popo", 5)
